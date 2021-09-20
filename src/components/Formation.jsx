3f650a7b2a35426ed1fb56a69ae52c76c5dedeb0
jkalebe <jeremiaskalebe@gmail.com>
2021-09-20 00:32:10 -0300

    Ajuste no title e no tamanho da font do navBar

--- src/components/Formation.jsx
+++ src/components/Formation.jsx
@@ -9,7 +9,7 @@ export default function Formation() {
                     <div id='divLiandImg'>
                     <div id="divLi">
                         <li><p>Formando Engenharia da Computação, Universidade Federal do Pará(2016-2021)</p></li>
-                        <li><p>Proposta de conclusão de curso: <a href="https://marcosamaris.github.io/laws/#/">"Laws - Ferramenta de anotação para gravações de midias!"</a></p></li>
+                        <li><p>Proposta de conclusão de curso: <a target='_blank' href="https://marcosamaris.github.io/laws/#/">"Laws - Ferramenta de anotação para gravações de midias!"</a></p></li>
                     </div>
                     <div id="divImg">
                         <img style={{width: '15rem'}} src={Pc} alt="PC" />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,14 +8,14 @@ export default class navBar extends Component {
 
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
+                <Navbar.Brand style={{fontSize:'2rem'}} href="#home">Portfólio</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
+                <Nav style={{fontSize:'1.5rem'}} className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#formation">Formação</Nav.Link>
                     <Nav.Link href="#xperiencia">Experiencia</Nav.Link>
-                    <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                    <Nav.Link href="#portfolio">Portfólio</Nav.Link>
                     <Nav.Link href="#contact">Contato</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
